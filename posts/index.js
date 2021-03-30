752eb401dca6baca1a5baeaf4bf5155b5e649d71
Nico Del Rosario <stormcm6@gmail.com>
2021-03-30 16:26:48 +0800

    3 - posts service creation

---
+++ posts/index.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const { randomBytes } = require('crypto');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.json());
+
+// store every post created
+const posts = {};
+
+// Routes
+
+// Get posts
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+// Craete new post
+app.post('/posts', (req, res) => {
+  // random ID
+  const id = randomBytes(4).toString('hex');
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
+});
+
+app.listen(4000, () => {
+  console.log('Listening on 4000');
+});
