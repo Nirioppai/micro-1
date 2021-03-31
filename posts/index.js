c755ec8d70bdd1f9501ede6e9a563613fd213de0
Nico Del Rosario <stormcm6@gmail.com>
2021-03-31 16:43:38 +0800

    20 - emitting events

--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const { randomBytes } = require('crypto');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const { default: axios } = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -18,7 +19,7 @@ app.get('/posts', (req, res) => {
 });
 
 // Craete new post
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   // random ID
   const id = randomBytes(4).toString('hex');
   const { title } = req.body;
@@ -27,6 +28,14 @@ app.post('/posts', (req, res) => {
     id,
     title,
   };
+
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: {
+      id,
+      title,
+    },
+  });
 
   res.status(201).send(posts[id]);
 });
